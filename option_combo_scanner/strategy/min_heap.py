ace18248557f99727e378a36cc857658f3089b34
Aryan . <aryan@algo-one.com>
2024-04-18 21:14:57 +0530

    Updated MinHeap Class with new methods

--- option_combo_scanner/strategy/min_heap.py
+++ option_combo_scanner/strategy/min_heap.py
@@ -26,14 +26,26 @@ class MinHeap:
 
     def update(self, old_item, new_item):
         try:
-                
+
             self.heap[self.heap.index(old_item)] = new_item
             heapq.heapify(self.heap)
         except Exception as e:
             print(f"Min Heap Exception: {e}")
 
+    def delete_item(self, config_id):
+        try:
+            for item in self.heap:
+                if item[1] == config_id:
+                    self.heap.remove(item)
+                    heapq.heapify(self.heap)
+                    return
+            # If config_id is not found in any item
+            raise ValueError("Configuration ID not found in heap")
+        except ValueError as e:
+            print(f"Min Heap Exception: {e}")
+
     def get_items(self):
         return sorted(self.heap)
-    
+
     def __len__(self):
         return len(self.heap)
